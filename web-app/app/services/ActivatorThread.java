c50b5b59c66527fedc171f3d5616ef08ca0273ce
LArbaszewski <lukaszarbaszewski@yahoo.com>
2016-11-16 00:43:40 +0100

    inactive accounts delete service

---
+++ web-app/app/services/ActivatorThread.java
@@ -0,0 +1,62 @@
+package services;
+
+import java.util.Date;
+import java.util.List;
+
+import models.UnactivatedAccount;
+import play.Logger;
+
+public class ActivatorThread extends Thread implements Runnable {
+
+	@Override
+	public void run() {
+		
+		while(true) {
+			
+			// TODO Auto-generated method stub
+			try {
+				Thread.sleep(5000);
+			} catch (InterruptedException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}		
+			
+		    long intervalInMs = 1000; // run every second
+		    long nextRun = System.currentTimeMillis() + intervalInMs;
+		    method();
+		    if (nextRun > System.currentTimeMillis()) {
+		        try {
+					Thread.sleep(nextRun - System.currentTimeMillis());
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		    }
+		}
+	}
+	
+	
+	public void method(){
+		
+		List<UnactivatedAccount> unactiv = UnactivatedAccount.find.all();
+	       
+		
+		Date dat = new Date(System.currentTimeMillis());
+		if (unactiv != null) {
+			Logger.info("Number of inactive accounts" +  unactiv.size());
+			for (int i = 0; i < unactiv.size(); i++) {
+				if (unactiv.get(i).expiredDate.compareTo(dat) > 0) {  
+					
+				}else{
+					unactiv.get(i).delete();
+					Logger.info("Deleted account");
+		           
+				
+				}
+			}
+		} 
+	}
+	
+	
+
+}
--- web-app/app/services/ApplicationTimer.java
+++ web-app/app/services/ApplicationTimer.java
@@ -2,8 +2,13 @@ package services;
 
 import java.time.Clock;
 import java.time.Instant;
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
 import java.util.concurrent.CompletableFuture;
 import javax.inject.*;
+
+import models.UnactivatedAccount;
 import play.Logger;
 import play.inject.ApplicationLifecycle;
 
@@ -27,24 +32,32 @@ public class ApplicationTimer {
     private final Clock clock;
     private final ApplicationLifecycle appLifecycle;
     private final Instant start;
-
+    private final ActivatorThread th;
+  
     @Inject
-    public ApplicationTimer(Clock clock, ApplicationLifecycle appLifecycle) {
+    public ApplicationTimer(Clock clock, ApplicationLifecycle appLifecycle, ActivatorThread th) {
         this.clock = clock;
         this.appLifecycle = appLifecycle;
+        this.th = th;
+        
+        th.start();
+        
         // This code is called when the application starts.
         start = clock.instant();
         Logger.info("ApplicationTimer demo: Starting application at " + start);
+        
 
         // When the application starts, register a stop hook with the
-        // ApplicationLifecycle object. The code inside the stop hook will
+        // ApplicationLifecycle object. d code inside the stop hook will
         // be run when the application stops.
         appLifecycle.addStopHook(() -> {
             Instant stop = clock.instant();
             Long runningTime = stop.getEpochSecond() - start.getEpochSecond();
             Logger.info("ApplicationTimer demo: Stopping application at " + clock.instant() + " after " + runningTime + "s.");
+           
             return CompletableFuture.completedFuture(null);
         });
     }
+
 
 }
